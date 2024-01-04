9aa5e059110c6a4e336c056c6e0eb8924ec9bf02
kyleuniversities <kyleuniversities@gmail.com>
2024-01-03 21:52:54 -0800

    initialized backend express app

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+// Set up express
+const app = express();
+
+// Set up express middleware
+app.use(jsonParser);
+app.use(express.json());
+app.use(cors());
+app.use('/', router);
+
+// Set up port data
+port = 5000;
+
+// Set up test endpoint
+router.get('/hello', function (req, res) {
+  return res.send(`Hello World!`);
+});
+
+// Launch app
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}.`);
+});
+
+// Export app
+module.exports = app;
